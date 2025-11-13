7c20bac56f240f0d4bc4b742444ee79e3c159775
francobm06 <francobmaranhao@gmail.com>
2025-11-13 12:32:05 -0300

    feat: Documentação javadoc Cliente.java e ClienteSocket.java
    Resolvi fazer os dois juntos porque são muito pequenos.
    Com isso, todo o pacote "principal" está documentando para javadoc. Vou partir para o "modelo".

--- src/main/java/principal/Cliente.java
+++ src/main/java/principal/Cliente.java
@@ -1,6 +1,13 @@
+// Documentado para javadoc
+// (apesar de não ter nada de muito util para comentar aqui)
+
 package principal;
 
 
+/**
+ * Representa um cliente no sistema
+ * Armazena informações relacionadas ao cliente
+ */
 public class Cliente {
     
 }
--- src/main/java/principal/socket/ClienteSocket.java
+++ src/main/java/principal/socket/ClienteSocket.java
@@ -1,5 +1,12 @@
+// Documentado para javadoc
+
 package principal.socket;
 
+/**
+ * Inicializa o cliente via socket
+ * Contém o método principal, ({@code main}), que inicializa o cliente e exibe
+ * uma mensage de 'Hello World' para teste
+ */
 public class ClienteSocket {
     public static void main(String[] args) {
         System.out.println("Hello World!");
