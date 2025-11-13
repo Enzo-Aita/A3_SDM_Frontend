fe289ea588519a68532a4c194eb5bea42e1bfd33
francobm06 <francobmaranhao@gmail.com>
2025-11-13 12:27:45 -0300

    feat: Documentação javadoc para Principal.java

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,12 +1,34 @@
+// Documentado para javadoc
 
 package principal;
 
 import modelo.Produto;
 import visao.FrmMenuPrincipal;
 
+/**
+ * Classe main/principal do sistema
+ * <p>
+ * Contém o método {@code main}
+ * Inicializa e exibe a janela principal do sistema, 
+ * {@link visao.FrmMenuPrincipal}
+ * </p>
+ * 
+ * <p>
+ * O pacote {@code principal} é responsável por iniciar a aplicação e coordenar
+ * a inicialização dos módulos
+ * </p>
+ */
 public class Principal {
 
-    
+    /**
+     * Método principal
+     * <p>
+     * Cria uma instância da tela principal, ({@link FrmMenuPrincipal}), e torna
+     * visível para o usuário
+     * </p>
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
         objetotela.setVisible(true);
