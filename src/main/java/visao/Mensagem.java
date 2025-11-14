62a502dc6a3bc89dc98b849fa54f8931f3ff6ac1
francobm06 <francobmaranhao@gmail.com>
2025-11-13 23:28:47 -0300

    feat: Documentação javadoc Mensagem.java

--- src/main/java/visao/Mensagem.java
+++ src/main/java/visao/Mensagem.java
@@ -1,8 +1,20 @@
+// Documentado para javadoc
 
 package visao;
 
-
+/**
+ * Representa uma exceção para exibir/mover mensagens na visão do aplicativo
+ * 
+ * <p>
+ * Estende {@link Exception}, permitindo personalização e "tratamento" das
+ * mensagens
+ * </p>
+ */
 public class Mensagem extends Exception {
+    /**
+     * Cria uma nova instância do {@code Mensagem} com o texto desejado
+     * @param mensagem Mensagem a ser enviada
+     */
     public Mensagem(String mensagem){
         super(mensagem);
     }
