761775e0f2d69825f60526c50b85dc164598d926
Enzo-Aita <Enzoaitanaime99@gmail.com>
2025-11-17 10:36:08 -0300

    style: alterado o codigo Categoria.java
    # Conflicts:
    #	.gitignore
    #	LICENSE
    #	README.md

--- src/main/java/modelo/Categoria.java
+++ src/main/java/modelo/Categoria.java
@@ -1,19 +1,19 @@
 package modelo;
 
-import dao.CategoriaDAO;
-import java.util.ArrayList;
+import java.io.Serializable;
 
 /**
- * Categoria de produto
- * Contém os atributos para identificação: nome, embalagem e tamanho
-*/
-public class Categoria {
+ * Categoria de produto Contém os atributos para identificação: nome, embalagem
+ * e tamanho
+ */
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private int id;
     private String nome;
     private String embalagem;
     private String tamanho;
-    private CategoriaDAO dao;
 
     /**
      * Construtor padrão
@@ -24,25 +24,26 @@ public class Categoria {
 
     /**
      * Construtor completo
-     * @param id    Código de identificação
-     * @param nome  Nome da categoria
-     * @param embalagem     Tipo de embalagem
-     * @param tamanho       Tamanho da embalagem
+     *
+     * @param id Código de identificação
+     * @param nome Nome da categoria
+     * @param embalagem Tipo de embalagem
+     * @param tamanho Tamanho da embalagem
      */
     public Categoria(int id, String nome, String embalagem, String tamanho) {
         this.id = id;
         this.nome = nome;
         this.embalagem = embalagem;
         this.tamanho = tamanho;
-        this.dao = new CategoriaDAO();
     }
 
     /**
      * Construtor Básico para categoria
-     * @param idCategoria   Código identificação da categoria
-     * @param nomeCategoria     Nome da categoria
+     *
+     * @param idCategoria Código identificação da categoria
+     * @param nomeCategoria Nome da categoria
      */
-    public Categoria(int idCategoria, String nomeCategoria){
+    public Categoria(int idCategoria, String nomeCategoria) {
         this.id = idCategoria;
         this.nome = nomeCategoria;
     }
@@ -103,80 +104,13 @@ public class Categoria {
         this.tamanho = tamanho;
     }
 
-    /**
-     * @return Retorna o objeto CategoriaDAO
-     */
-    public CategoriaDAO getDao() {
-        return dao;
-    }
-
-    /**
-     * @param dao Seta o objeto CategoriaDAO
-     */
-    public void setDao(CategoriaDAO dao) {
-        this.dao = dao;
-    }
-
-
-    /**
-     * Obtém a lista para todas as categorias registradas
-     * @return {@link Categoria}
-     */
-    public ArrayList<Categoria> getMinhaLista() {
-        return dao.getMinhaLista();
-    }
-
-    /**
-     * Adiciona uma nova categoria ao BD
-     * @param nome Nome da categoria
-     * @param embalagem Tipo de embalagem
-     * @param tamanho Tamanho da embalagem
-     * @return Retorna {@code true} se a função funcionar
-     */
-    public boolean insertCategoriaBD(String nome, String embalagem, String tamanho) {
-        int id = this.maiorID() + 1;
-        Categoria objeto = new Categoria(id, nome, embalagem, tamanho);
-        dao.insertCategoriaBD(objeto);
-        return true;
-    }
-    
-    /**
-     * Deleta uma categoria do BD
-     * @param id Código de Identificação da categoria a ser excluída
-     * @return Retorna {@code true} se a função funcionar
-     */
-    public boolean deleteCategoriaBD(int id) {
-        dao.deleteCategoriaBD(id);
-        return true;
-    }
-
-/**
- * Atualiza os dados de uma categoria
- * @param id Código de identificação da categoria
- * @param nome Novo nome
- * @param embalagem Nova embalagem
- * @param tamanho Novo tamanho
- * @return Retorna {@code true} caso a atualização funcione
- */    
-public boolean updateCategoriaBD(int id, String nome, String embalagem, String tamanho) {
-        Categoria objeto = new Categoria(id, nome, embalagem, tamanho);
-        return dao.updateCategoriaBD(objeto);
-    }
-    
-    /**
-     * Carrega uma categoria
-     * @param id Código de identificação da categoria
-     * @return Retorna o objeto {@link Categoria} correspondente
-     */
-    public Categoria carregaCategoria(int id) {
-        return dao.carregaCategoria(id);
-    }
-
-    /**
-     * Retorna o maior identificador no BD
-     * @return Retorna o maior valor de identificação
-     */
-    public int maiorID() {
-        return dao.maiorID();
+    @Override
+    public String toString() {
+        return "Categoria{"
+                + "id=" + id
+                + ", nome='" + nome + '\''
+                + ", embalgem='" + embalagem + '\''
+                + ",tamanho='" + tamanho + '\''
+                + '}';
     }
 }
